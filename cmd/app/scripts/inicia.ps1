8d761f5fd4df7014bbda7eb4bc6b67a3c645dfdd
brendatrindade <brendatrindde@gmail.com>
2025-05-22 09:24:24 -0300

    remove pwsh

--- cmd/app/scripts/inicia.ps1
+++
@@ -1,9 +0,0 @@
-docker-compose build
-docker-compose create
-docker ps -a
-docker-compose start servidor1
-docker-compose logs -f 
-#Para o veiculo:
-#docker-compose start veiculo
-#docker exec -it veiculo sh
-#./veiculo
--- cmd/app/scripts/para.ps1
+++
@@ -1,4 +0,0 @@
-docker-compose stop
-docker-compose down: remover containers criados
-docker-compose down --rmi all --volumes --remove-orphans
-clear
